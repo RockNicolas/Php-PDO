2f24c7a3b70250df0acc2686a373648e98aa1434
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-03-18 15:02:27 -0300

    ALteração na funcionabilidade

--- trasferir-ok.php
+++ trasferir-ok.php
@@ -2,12 +2,18 @@
     if (isset($_POST["textIDR"])){
         $conexão = new PDO('myslq:host=localhost;dhname=test', $user,  $pass);
 
-        $sql = "SELECT id FROM contas WHERE id = ".$_POST["txtIDD"];
-        $query = $conexão->query($sql)->fetchAll();
+        $sql = "SELECT COUNT(*) FROM contas WHERE id = :Destinado";
+        $prepareQuery = $conexão->prepare($sql);
+        $prepareQuery->bindParam(":Destinado", $_POST["txtIDD"]
+        , PDO:: PARAM_INT);
+        $prepareQuery->execute();
 
         if(count($query) > 0){
             $sql = "SELECT id FROM contas WHERE id = ".$_POST["txtIDR"];
-            $query = $conexão->query($sql)->fetchAll();
+            $prepareQuery = $conexão->prepare($sql);
+            $prepareQuery->bindParam(":Destinado", $_POST["txtIDD"]
+            , PDO:: PARAM_INT);
+            $prepareQuery->execute();
         
             if(count($query) > 0){
                     $sql = "SELECT saldo FROM contas WHERE id = ".$_POST["txtIDR"];

15954bbacc0e1f53229719091d36a848fb6a2bf5
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-03-18 14:18:17 -0300

    Criação da conexão e minhas tables

---
+++ index.php
@@ -0,0 +1,30 @@
+<h1>Banco dinheiro</h1>
+<table width="100%" border="1">
+<tr>
+    <td>ID</td>
+    <td>CLiente</td>
+    <td>Ação</td>
+    <td>Saldo</td>
+    <td>Transferir</td>
+</tr>
+</table>
+
+<?php
+    try{
+        $conexão = new PDO('myslq:host=localhost;dhname=test', $user,  $pass);
+
+        $sql = "SELECT * FROM contas ORDER BY id ASC";
+
+        foreach($conexão->query($sql) as $row){
+            echo "<tr>";
+            echo  "<td>".$row["id"]."<td>";
+            echo  "<td>".$row["nome"]."<td>";
+            echo  "<td>".$row["saldo"]."<td>";
+            echo  "<td><a hreaf='transferencia.php'>Transferir Valor</td>";
+            echo "</tr>";
+        }
+
+    } catch (PDOException $e){
+        echo "Ocorreu um erro:" .$e->getMessage();
+    }
+?>
